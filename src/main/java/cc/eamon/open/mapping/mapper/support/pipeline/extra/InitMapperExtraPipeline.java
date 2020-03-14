37776e8f1ec73a58147e92b87adb6d039bcb97aa
ymxiong@whu.edu.cn <ymxiong@whu.edu.cn>
2020-03-15 03:31:38 +0800

    refactor: pipeline of extra

--- src/main/java/cc/eamon/open/mapping/mapper/support/pipeline/extra/InitMapperExtraPipeline.java
+++ src/main/java/cc/eamon/open/mapping/mapper/support/pipeline/extra/InitMapperExtraPipeline.java
@@ -2,10 +2,16 @@ package cc.eamon.open.mapping.mapper.support.pipeline.extra;
 
 import cc.eamon.open.mapping.mapper.structure.item.MapperField;
 import cc.eamon.open.mapping.mapper.structure.item.MapperType;
+import cc.eamon.open.mapping.mapper.support.MapperEnum;
 import cc.eamon.open.mapping.mapper.support.pipeline.BasePipeline;
-import cc.eamon.open.mapping.mapper.support.pipeline.Pipeline;
+import cc.eamon.open.mapping.mapper.support.strategy.ModifyStrategy;
+import cc.eamon.open.mapping.mapper.support.strategy.RenameStrategy;
+import cc.eamon.open.mapping.mapper.util.ClassUtils;
 import com.squareup.javapoet.FieldSpec;
+import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
+
+import javax.lang.model.element.Modifier;
 
 /**
  * Author: eamon
@@ -14,19 +20,32 @@ import com.squareup.javapoet.TypeSpec;
  */
 public class InitMapperExtraPipeline extends BasePipeline {
 
-    public InitMapperExtraPipeline(Pipeline pipeline) {
-        super(pipeline);
+    public InitMapperExtraPipeline() {
+        super(null);
     }
 
     @Override
     public TypeSpec.Builder buildTypeBefore(MapperType type, TypeSpec.Builder typeSpec) {
-
         return typeSpec;
     }
 
     @Override
     public FieldSpec.Builder buildSelfField(MapperField field, FieldSpec.Builder fieldSpec) {
+        RenameStrategy renameStrategy = (RenameStrategy) field.getStrategies().get(MapperEnum.RENAME.getName());
+        ModifyStrategy modifyStrategy = (ModifyStrategy) field.getStrategies().get(MapperEnum.MODIFY.getName());
 
+
+        if (field.getList()) {
+            fieldSpec = FieldSpec.builder(
+                    ClassUtils.getParameterizedList(TypeName.get(modifyStrategy.getModifyType())),
+                    renameStrategy.getName(),
+                    Modifier.PUBLIC);
+        } else {
+            fieldSpec = FieldSpec.builder(
+                    TypeName.get(modifyStrategy.getModifyType()),
+                    renameStrategy.getName(),
+                    Modifier.PUBLIC);
+        }
         return fieldSpec;
     }
 
