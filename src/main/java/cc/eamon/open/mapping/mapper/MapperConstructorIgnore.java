1570aa80a2fa9b295134c311876279366c9f05b7
ymxiong@whu.edu.cn <ymxiong@whu.edu.cn>
2020-07-18 17:16:02 +0800

    merge & not fix: from zjy

--- src/main/java/cc/eamon/open/mapping/mapper/MapperConstructorIgnore.java
+++ src/main/java/cc/eamon/open/mapping/mapper/MapperConstructorIgnore.java
@@ -1,5 +1,12 @@
 package cc.eamon.open.mapping.mapper;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.TYPE})
+@Retention(RetentionPolicy.SOURCE)
 public @interface MapperConstructorIgnore {
 
     String[] value() default {};
